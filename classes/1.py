efe6023edabb25ab28d1f673db2448a7e4ce476a
Balzhan31 <balzhan0305@icloud.com>
2024-02-09 19:26:41 +0600

    done

--- classes/1.py
+++ classes/1.py
@@ -1,1 +1,11 @@
-print("leehi")
+class str:
+    def __init__(self):
+        self.str=""
+    def getstr(self):
+        self.str=input()
+    def printstr(self):
+        print(self.str.upper())
+
+x=str()
+x.getstr()
+x.printstr()
---
+++ functions1/1.py
@@ -0,0 +1,4 @@
+def o(gramm):
+    #print(gramm/28.3495231)
+    print(gramm*28.3495231) 
+o(float(input()))
